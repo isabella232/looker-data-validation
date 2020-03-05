c3c1a8734dd4ef36f253cfb49e4f30e2e7e6b67e
Sandeep Singh <sandeepsingh@Sandeeps-MacBook-Pro.local>
2020-03-04 16:49:59 -0800

    updating script

--- sql_writer_tests.sh
+++ sql_writer_tests.sh
@@ -4,7 +4,8 @@ export PARALLEL_SPLIT_TEST_PROCESSES=15
 bundle install
 cd spec
 rm -rf results
-mkdir -p results/data/sql
+mkdir -p results/data
+mkdir -p results/sql
 mkdir -p results/reports
 echo 'Hang in there, running tests, this might take a while'
 parallel_split_test run_tests.rb --format html --out results/reports/results.html
